9ba67b4b79b3f265396e4bd14294274f40115c50
Sandhya <sandhyach062@gmail.com>
2025-09-14 20:45:29 +0545

    Initial Commit

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
-# import os
-# from dotenv import load_dotenv
-# import chainlit as cl
-# import google.generativeai as genai 
-
-# load_dotenv()
-
-# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-# model = genai.GenerativeModel("gemini-2.0-flash")
-
-# @cl.on_message
-# async def message_handler(message: cl.Message):
-#     user_input = message.content 
-#     response = model.generate_content(user_input)
-#     await cl.Message(content=response.text).send()
-
 
 
 import os
